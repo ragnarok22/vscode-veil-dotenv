11e59521bc2b782672c8cb8b79279db03356239e
Reinier Hernández <sasuke.reinier@gmail.com>
2026-01-22 10:41:01 -0300

    chore(prettier): integrate formatting tooling and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,32 @@
 import typescriptEslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier/recommended";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
+export default [
+    {
+        files: ["**/*.ts"],
     },
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint.plugin,
+        },
 
-    languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+        languageOptions: {
+            parser: typescriptEslint.parser,
+            ecmaVersion: 2022,
+            sourceType: "module",
+        },
+
+        rules: {
+            "@typescript-eslint/naming-convention": ["warn", {
+                selector: "import",
+                format: ["camelCase", "PascalCase"],
+            }],
+
+            curly: "warn",
+            eqeqeq: "warn",
+            "no-throw-literal": "warn",
+            semi: "warn",
+        },
     },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+    prettier
+];
